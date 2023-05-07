c162a85866245c69acab2e338d0d3f7911551b9d
Naidu Rongali <rongalinaidu@gmail.com>
2023-05-07 11:49:16 -0700

    Update Arrays_Examples.py

--- Arrays_Examples.py
+++ Arrays_Examples.py
@@ -1,1 +1,34 @@
+#Document Strings for Methods and Classes
+#
+# For comments
+/*
+* Multi line comments
+*/
+"""
+Using List as Array
+"""
+
+#Creating Array
+sample = [1,2,4]
+
+#printing Array
+sample
+
+#Accessing the Element
+sample[0]
+
+#iterating the elements
+for i in sample:
+    pront i
+
+#Searching the Element
+
+if 20 in sample:
+    print("the number is present in the Array")
+    
+#Adding Element
+sample.append(5)
+
+#Deleting the Element
+del sample[0]
 
